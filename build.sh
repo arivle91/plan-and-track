768c0ab1d8121e9067620ce3ebe5f4a503fc7677
User Elvira <arivle27@gmail.com>
2025-05-20 19:16:19 +0200

    Add build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-set -o errexit
+#set -o errexit
+#!/bin/bash
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+echo "Running collectstatic..."
+python manage.py collectstatic --noinput
+
+echo "Running database migrations..."
 python manage.py migrate
+
+#pip install -r requirements.txt
+#python manage.py collectstatic --no-input
+#python manage.py migrate
