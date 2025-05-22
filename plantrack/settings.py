a6dcdfecd026444025a20dd760d20247ab3c4a6e
User Elvira <arivle27@gmail.com>
2025-05-22 15:28:40 +0200

    Use SQLite by default when DATABASE_URL is not set

--- plantrack/settings.py
+++ plantrack/settings.py
@@ -79,17 +79,18 @@ WSGI_APPLICATION = 'plantrack.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-    # 'default':
-    # {
-    # 'ENGINE': 'django.db.backends.sqlite3',
-    # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+# 'default': dj_database_url.config(
+#     default=os.environ.get('DATABASE_URL'),
+#     conn_max_age=600,
+#     ssl_require=True
+# )
+# 'default':
+# {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
+# }
 
 
 # Password validation
